bd8a31ff33f4caa5e32443c804351ae75f622970
gammawave-geek <136863100+gammawave-geek@users.noreply.github.com>
2023-07-08 23:06:12 -0500

    initial version (#1)
    * initial commit
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    * modifying workflow
    
    ---------
    
    Co-authored-by: Scott Evers <scottaevers+github@gmail.com>

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "AWS_DEFAULT_REGION" {
+  type = string
+}
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "BUCKET_NAME" {
+  type = string
+}
